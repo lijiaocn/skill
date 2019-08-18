918ad8fd9cedf37f176a4135988ef1337f0deccf
lijiaocn <lijiaocn@foxmail.com>
2019-08-18 17:31:00 +0800

    m

---
+++ run.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+#
+# run.sh
+# Copyright (C) 2018 lijiaocn <lijiaocn@foxmail.com>
+#
+# Distributed under terms of the GPL license.
+#
+
+port=`cat port`
+#nohup bundle exec jekyll serve --port $port --incremental 2>&1 1 > /tmp/jekyll.log &
+nohup bundle exec jekyll serve --port $port  2>&1 1 > /tmp/jekyll.log &
+#bundle exec jekyll serve --port `cat port`
